ff1f486401a3e186fd512f24833ce7e156a7f8d4
priyadegwekar <101189065+priyadegwekar@users.noreply.github.com>
2022-04-30 16:58:48 +0530

    30th April "first commit"

---
+++ index.php
@@ -0,0 +1,312 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clever Course</title>
+        <meta name="keywords" content="HTML, CSS, JavaScript"> 
+        <meta name="description" content="Learning HTML CSS">
+        <meta name="author" content="Priya Degwekar"> 
+        <!-- <meta http-equiv="refresh" content="30"> -->
+        <meta charset="utf-8">  
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">  
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/bootstrap.css.map">
+        <link rel="stylesheet" href="./css/all.css">
+        <link rel="stylesheet" href="./css/all.min.css">
+        <link rel="stylesheet" href="./css/fontawesome.css">
+        <link rel="stylesheet" href="./css/fontawesome.min.css">
+        <link rel="stylesheet" type="text/css" href="./css/owl.carousel.min.css">
+    </head>
+    <body class="home">
+        <header class="header">
+            <div class="container">
+                <div class="row topnav">
+                    <div class="col-md-6 leftnav">
+                        <i class="fa-solid fa-phone"></i>1800-222-222
+                        <i class="fa-solid fa-envelope"></i> contact@clevercoursewptheme.com  
+                    </div>
+                    <div class="col-md-6 rightnav">
+                        <div class="social-icon">
+                                <i class="fa-brands fa-facebook-f"></i>
+                                <i class="fa-brands fa-google-plus-g"></i>
+                                <i class="fa-brands fa-flickr"></i>
+                                <i class="fa-brands fa-tumblr"></i>
+                                <i class="fa-brands fa-linkedin"></i>
+                                <i class="fa-brands fa-twitter"></i>
+                                <i class="fa-brands fa-vimeo-v"></i>
+                                
+
+                        </div>
+                        <div class="sign-in">
+                                <i class="fa-solid fa-lock"></i>Sign In 
+                                <a class="sign-up" href="#">Sign Up</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"><img src="./images/logo.png" alt="Logo"></a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">HOME</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">LMS</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">COURSES</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul> 
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">PAGES</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">BLOG</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">PORTFOLIO</a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">SHORTCODES</a>
+                        </li>
+                        <li class="search-icon">
+                            <i class="fa fa-search"></i>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="banner-sider">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="./images/slider-1.jpg" alt="First slide">
+                            <div class="carousel-caption d-none d-md-block caption-one">
+                                <h2>Onsite</h2> 
+                                <h1>Course</h1>
+                                <p>Meet new friends and study with experts</p>
+                                <button class="learn-btn">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="./images/slider-2.jpg" alt="Second slide">
+                            <div class="carousel-caption d-none d-md-block caption-two">
+                                <h1>The Best Solution</h1><br>
+                                <h3>To create and sell online courses</h3>
+                                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                                <button class="learn-btn">Learn More</button>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="./images/slider-3.jpg" alt="Third slide">
+                            <div class="carousel-caption d-none d-md-block caption-three">
+                                <h1>Online Course</h1>
+                                <h5>Improve your skills</h5>
+                                <button class="checkout-btn">Checkout</button>
+                            </div>
+                        </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </section>
+        <section class="static-blocks">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="./images/icon-1.png" alt="icon" class="static-img">
+                        <h3 class="static-title">MAGNA RISUS DAPIBUS</h3>
+                        <p class="static-content">Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="./images/icon-2.png" alt="icon" class="static-img">
+                        <h3 class="static-title">PURUS NULLAM JUSTO</h3>
+                        <p class="static-content">Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis.</p> 
+                    </div>
+                    <div class="col-md-3">
+                        <img src="./images/icon-3.png" alt="icon" class="static-img">
+                        <h3 class="static-title">VULPUTATE CRAS</h3>
+                        <p class="static-content">Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis.</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="./images/icon-4.png" alt="icon" class="static-img">
+                        <h3 class="static-title">ULTRICIES EGESTA</h3>
+                        <p class="static-content">Nullam quis risus eget urna mollis ornare vel eu leo. Sed posuere consectetur est at lobortis.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="learn-more">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="learn-title">RISUS ULTRICIES MAGNA</h3>
+                        <p class="learn-content">Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean lacinia bibendum nulla.</p>
+                    </div>
+                    <div class="col-md-6 text-lg-left">
+                       <button class="learn-btn">Learn more</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="search-courses">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="heding-content">
+                        <h1 class="text-center">Search for courses</h1>
+                        <p class="text-center">Fill keywords to seek for courses</p>
+                        </div>
+                        <form action="/examples/actions/confirmation.php" method="post">
+                            <div class="row align-items-center g-3 search-form">
+                                <div class="col-auto">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <select class="form-select" aria-label="Default select example">
+                                        <option selected>Open this select menu</option>
+                                        <option value="1">One</option>
+                                        <option value="2">Two</option>
+                                        <option value="3">Three</option>
+                                    </select>
+                                </div>
+                                <div class="col-auto">
+                                    <label class="visually-hidden" for="inputEmail">Email</label>
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Email" required>
+                                </div>
+                            <div class="col-auto">
+                                <button type="submit" class="btn btn-primary">Search</button>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="poplular-course">
+            <div class="head">
+                <h3 class="text-center">CHECK OUT SOME POPULAR COURSES</h1>
+                <p class="text-center">Tellus Elit Euismod Ultricies</p>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="./images/banner-1.jpg" alt="">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="./images/banner-2.jpg" alt="">
+                    </div>
+                    <div class="col-md-4">
+                        <img src="./images/banner-3.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="contact-us">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="contact-title">FEEL FREE TO ASK US</h3>
+                        <p class="contact-content">If you have any questions, please feel free to drop us questions</p>
+                    </div>
+                    <div class="col-md-6 text-lg-left">
+                       <button class="contact-btn">Contact us</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="simplecourse-footer">
+            <div class="container">
+                <div class="footer-first">
+                    <div class="row">
+                            <div class="col-md-3">
+                                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            </div>
+                    </div>
+                </div>
+                <div class="footer-second">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>Theme By GoodLayers</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p class="copyright-right">Copyright © 2014 - All Right Reserved - GoodLayers inc</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </footer>
+        
+
+
+    <script src="./js/script.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/bootstrap.js"></script>
+    <script src="./js/all.js"></script>
+    <script src="./js/all.min.js"></script>
+    <script src="./js/fontawesome.js"></script>
+    <script src="./js/fontawesome.min.js"></script>
+    <script src="./js/owl.carousel.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script> 
+    </body>
+    
+</html>
